0d4303435bf8514fabe8f638959723e872d7a2f8
Jake Thornberry <wjthornberry@gmail.com>
2017-07-29 22:27:05 -0500

    Add requirements of app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Requires use of basic flashcard
+var basicFlashCard = require('./basic-flashcard.js');
+
+// Requires use of cloze flashcard
+var clozeFlashcard = require('/cloze-flashcard.ks');
+
+// Requires inquirer package – gathers user input
+var inquirer = require('inquirer');
+
+// Requires fs – File System
+var fs = require('fs');
