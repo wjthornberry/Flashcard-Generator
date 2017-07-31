0b00b226847e2fe53e202ad03470a95c28398488
Jake Thornberry <wjthornberry@gmail.com>
2017-07-30 20:54:22 -0500

    2nd draft of basic card

--- basic-card.js
+++ basic-card.js
@@ -1,26 +1,8 @@
-// Requires fs
-var fs = require('fs');
-
-// Constructor
-function BasicFlashcard(front, back) {
+// Constructor function for the basic flashcard
+function BasicCard(front, back) {
     this.front = front;
     this.back = back;
-    this.create = function() {
-        // The basic flashcard object to be appended
-        // to file
-        var data = {
-            front: this.front,
-            back: this.back,
-            type: 'basic',
-        };
-        // Adds card to log.txt
-        fs.appendFile('log.txt', JSON.stringify(data) + ';' 'utf8', function(err) {
-            // If there is an error, log it
-            if (err) {
-                console.log(err);
-            }
-        })
-    };
-}
 
-module.exports = BasicFlashcard;
+};
+
+module.exports = BasicCard;
